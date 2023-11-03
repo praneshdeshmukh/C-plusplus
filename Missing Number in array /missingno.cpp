7768433bac50c0982f92673ee52c025a54801aba
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-11-03 23:17:09 +0530

    Update missingno.cpp

--- Missing Number in array /missingno.cpp
+++ Missing Number in array /missingno.cpp
@@ -1,4 +1,10 @@
- int sortAndFindNo(vector<int>& nums) {
+#include<iostream>
+#include <vector>
+#include <algorithm> // for sort func()
+using namespace std;
+
+      int sortAndFindNo(vector<int>& nums) {
+       
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
@@ -14,7 +20,7 @@
         return counter;
     }
 
-    int xorr(vector<int>& nums) {
+     int xorr(vector<int>& nums) {
 
         int ans=0;
 
@@ -22,10 +28,12 @@
         for(int i=0;i<nums.size();i++) {
             ans= ans ^ nums[i];
         }
+     
         // 2. xor all given range indexes 
         // (including the missing no index)
         // '<=' and not only '<' bcz the given range is inclusive
         //eg [0,3] means -> [0,1,2,3] (0 & 3 included)
+     
         for(int i=0; i<=nums.size(); i++) {
             ans = ans ^ i;
         }
@@ -34,21 +42,21 @@
     }
     int missingNumber(vector<int>& nums) {
       
-    //  int sum=0;
-    //  int n=nums.size();
-    //  for(int index=0; index<n; index++) {
-    //      sum = sum + nums[index];
-    //  }
-    //  int total = ((n)*(n+1)) / 2;
-    //  int ans = total - sum;
+     int sum=0;
+     int n=nums.size();
+     for(int index=0; index<n; index++) {
+         sum = sum + nums[index];
+     }
+     int total = ((n)*(n+1)) / 2;
+     int ans = total - sum;
 
-    //  return ans;
+     return ans;
     }
-};
 
 int main() {
-  int nums = [3,0,1];
-  int res=0;
+
+    vector<int> nums = {3, 0, 1};
+    int res=0;
   // APPROACH 1 -
   // res = sortAndFindNo(nums);
 
