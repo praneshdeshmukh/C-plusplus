c24770e9d045ebc4d82185245086092c0090f008
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-11-07 23:32:07 +0530

    d8

--- find missing elements in array of duplicates /findel.cpp
+++ find missing elements in array of duplicates /findel.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// METHOD 1 : SORTING + SWAPPING APPROACH
 void findMissingElementsInArrayofDuplicates(int *arr, int n) {
     
     int i=0;
@@ -33,12 +34,38 @@ void findMissingElementsInArrayofDuplicates(int *arr, int n) {
     cout<<endl;
 }
 
+// METHOD 2 : NEGATIVE MARKING APPROACH
+void usingNegativeMarkApproach(int *arr, int n) {
+    for(int i=0; i<n; i++) {
+        int index= abs(arr[i]);
+        
+        // if not visited yet mark it visited(i.e make it -ve)
+        if(arr[index - 1] > 0) {
+            arr[index - 1] = arr[index - 1] * -1;
+        }
+    }
+        // printing array uptil now
+        // for(int k=0; k<n; k++) {
+        //     cout<< arr[k] << " ";
+        // }
+        // cout<<endl;
+        
+        // iterate through arr again and check if there is any unvisited index
+        // i.e is ther any +ve index 
+        for(int i=0; i<n; i++) {
+            if(arr[i] > 0) {
+                cout<< i+1 << " ";
+            }
+        }
+        cout << endl;
+}
 int main()
 {
     int arr[] = {1,5,4,4,5};
     int n = sizeof(arr) / sizeof(int);
     
-    findMissingElementsInArrayofDuplicates(arr,n); // o/p - 2 3 
+    findMissingElementsInArrayofDuplicates(arr , n); // o/p : 2 3 
+    //usingNegativeMarkApproach(arr , n) // o/p : 2 3
     
     return 0;
 }
