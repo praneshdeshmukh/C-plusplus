4654d5413f43eb98a4fec92e431304fee2590f27
Pranesh Deshmukh <praneshd2003@gmail.com>
2024-01-19 22:59:11 +0530

    Update 1910Remove All Occurrences of a Substring.cpp

--- Chars and Srings /Strings/1910Remove All Occurrences of a Substring.cpp
+++ Chars and Srings /Strings/1910Remove All Occurrences of a Substring.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
        int pos = s.find(part);
+        // pos == npos =>  substring does not exist in original string
+        // pos != npos => substring exist
        while(pos != string::npos) {
            s.erase(pos, part.size());
            pos=s.find(part);
